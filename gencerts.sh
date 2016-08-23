04c8b8212eae157c26018a000ae4cc681b8a043e
Hugo Bandeira Tavares <hugo.bandeira.tavares@celfocus.com>
2016-08-23 16:33:54 +0100

    UPD: License / Readme file ADD: Certificate generator

---
+++ gencerts.sh
@@ -0,0 +1,15 @@
+for DOMAIN in *; do
+    if [ -d "${DOMAIN}" ]; then
+    	echo "INFO Generating KEY, CSR and CRT for ${DOMAIN}"
+    	echo "###"
+    	echo "INFO Generating private key for ${DOMAIN}: ${DOMAIN}.key"
+    	openssl genrsa -out ${DOMAIN}/${DOMAIN}.key 2048
+    	echo "INFO Generating CSR for ${DOMAIN}: ${DOMAIN}.csr"
+		openssl req -new -sha256 -key ${DOMAIN}/${DOMAIN}.key -out ${DOMAIN}/${DOMAIN}.csr
+    	echo "INFO Generating SSL for ${DOMAIN}: ${DOMAIN}.crt"
+        openssl x509 -signkey ${DOMAIN}/${DOMAIN}.key -in ${DOMAIN}/${DOMAIN}.csr -req -days 365 -out ${DOMAIN}/${DOMAIN}.crt
+        echo -e "\n"
+        echo "INFO Gerenated files for ${DOMAIN}"
+        ls -lrt ${DOMAIN}
+	fi
+done
